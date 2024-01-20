508584219421eed8e158760c6dfe64994420d306
game.desktop <game.desktop@example.com>
2024-01-20 19:11:35 +0800

    添加首页

--- src/views/public/header.vue
+++ src/views/public/header.vue
@@ -5,16 +5,34 @@
         <div class="logo">imago</div>
       </div>
       <div class="flex flex-row">
-        <div>关于</div>
+        <div class="cursor">关于</div>
         <span class="split">|</span>
-        <div class="cursor">退出</div>
+        <div class="cursor" @click="goLogin">登录</div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
+<script>
+import {defineComponent} from "vue";
+import {useRouter} from "vue-router";
 
+let router = null
+
+const goLogin = () => {
+  console.log('CCC')
+  router.push(`/login?from=index`)
+}
+
+export default defineComponent({
+  setup() {
+    router = useRouter()
+
+    return {
+      goLogin,
+    }
+  }
+})
 </script>
 
 <style>
@@ -30,14 +48,18 @@
   flex-direction: row;
 }
 
-.header{
+.cursor {
+  cursor: pointer;
+}
+
+.header {
   height: 76px;
   line-height: 76px;
   justify-content: space-between;
 
   background-color: #f9fafb;
-  padding: 0 100px 0 100px ;
-  box-shadow: 0 0 8px rgba(0,0,0,0.2);
+  padding: 0 100px 0 100px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
 
   .logo {
     font-size: 36px;
